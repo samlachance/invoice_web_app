b043446d44fdee7a45a78e133bbc73029a9db02e
Sam Lachance <slachance@gmail.com>
2016-01-23 16:01:11 -0500

    TimeEntries now add to client's balance

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -2,6 +2,11 @@ class TimeEntry < ActiveRecord::Base
   belongs_to :client
   belongs_to :developer
 
-  def initialize
-    
+  def initialize(args)
+    super
+    total = self.time * self.rate
+    client = self.client
+    old_balance = client.balance
+    client.update_attribute(:balance, old_balance + total)
+  end
 end
